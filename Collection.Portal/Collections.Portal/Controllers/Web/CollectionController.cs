1b7f42034228fa5f11ae2c87e0783ee51ea280ff
DanHulse <daniel.hulse@theleadagency.com>
2017-04-30 18:23:11 +0100

    Added Movie View with modal form and bootstrap

---
+++ Collection.Portal/Collections.Portal/Controllers/Web/CollectionController.cs
@@ -0,0 +1,39 @@
+﻿using Collections.Portal.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Portal.Controllers.Web
+{
+    public class CollectionController : Controller
+    {
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieViewModel model)
+        {
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            return View();
+        }
+
+        public IActionResult Albums()
+        {
+            return View();
+        }
+    }
+}
---
+++ Collection.Portal/Collections.Portal/ViewModels/MovieViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Portal.ViewModels
+{
+    public class MovieViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string TranslatedTitle { get; set; }
+
+        public string Directors { get; set; }
+
+        public string Producers { get; set; }
+
+        public string Writers { get; set; }
+
+        public string Cast { get; set; }
+
+        public string Genres { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public int? RunningTime { get; set; }
+
+        public string Certification { get; set; }
+
+        public string Formats { get; set; }
+
+        public bool BluRayFormat { get; set; }
+
+        public bool DVDFormat { get; set; }
+
+        public bool DigitalFormat { get; set; }
+
+        public string Series { get; set; }
+
+        public int? SeriesNumber { get; set; }
+
+        public bool Watched { get; set; }
+
+        public int? Rating { get; set; }
+
+        public string PosterUrl { get; set; }
+    }
+}
